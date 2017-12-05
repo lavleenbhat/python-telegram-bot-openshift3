942ba7a3e77b8974da40ebb76b7c018afa3f8bc4
lavleenbhat <33768388+lavleenbhat@users.noreply.github.com>
2017-12-05 15:03:23 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Updater, Fi
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
-TOKEN = '12345'
+TOKEN = '506359509:AAHi33eJm84g4cFcUm0zfOalx_Cr-96B6Zs'
 
 
 def start(bot, update):
